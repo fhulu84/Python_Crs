ab0eb2dc32035b9fced3577c2a1dee2a63f70d6b
fhulu84 <fhulu84@users.noreply.github.com>
2020-02-07 23:48:46 +0000

    Changes

--- Scripting/SMS/sms.py
+++ Scripting/SMS/sms.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client 
  
-account_sid = 'AC4d08180e8e91813fb359290c0ad46839' 
-auth_token = '7643e22a9c602f5962674aebffa7320b' 
+account_sid = [Accound_SID]
+auth_token = [AuthToken]
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
